23b854217398e891e57d92fa5e64f77541048be9
texastesla01-creator <texastesla01@gmail.com>
2026-04-09 09:30:59 -0500

    Add LipidIQ page and update nav/routing

--- artifacts/rxbuilds/src/App.tsx
+++ artifacts/rxbuilds/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -8,6 +8,7 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import Home from "@/pages/Home";
 import TaxRx from "@/pages/TaxRx";
+import LipidIQ from "@/pages/LipidIQ";
 import PrivacyPolicy from "@/pages/PrivacyPolicy";
 import EULA from "@/pages/EULA";
 import Contact from "@/pages/Contact";
@@ -15,6 +16,9 @@ import Contact from "@/pages/Contact";
 const queryClient = new QueryClient();
 
 function Router() {
+  const [location] = useLocation();
+  const hideGlobalFooter = location === "/lipidiq";
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
@@ -22,13 +26,14 @@ function Router() {
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/taxrx" component={TaxRx} />
+          <Route path="/lipidiq" component={LipidIQ} />
           <Route path="/privacy-policy" component={PrivacyPolicy} />
           <Route path="/eula" component={EULA} />
           <Route path="/contact" component={Contact} />
           <Route component={NotFound} />
         </Switch>
       </main>
-      <Footer />
+      {!hideGlobalFooter && <Footer />}
     </div>
   );
 }
